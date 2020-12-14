a7f6dba58d62552ca4673bb8d7b65212a67f3577
Brandon Cermak <cermakprofessional@gmail.com>
2020-12-14 15:37:45 -0600

    trying to get API to work on firebase

--- src/components/utils/API.js
+++ src/components/utils/API.js
@@ -3,6 +3,9 @@ import axios from "axios";
 export default {
   // Gets all users
   getUsers: function() {
+    {
+      baseURL:'https://empdirr-default-rtdb.firebaseio.com/'
+    }
     return axios.get("https://randomuser.me/api/?results=200&nat=us");
   }
 };
