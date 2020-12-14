4f210dc69453bb41cb9d2cb52f2d17ddaf396792
Brandon Cermak <cermakprofessional@gmail.com>
2020-12-14 15:39:16 -0600

    changing

--- src/components/utils/API.js
+++ src/components/utils/API.js
@@ -1,11 +1,15 @@
 import axios from "axios";
 
+export default axios.create(
+  {
+    baseURL:'https://empdirr-default-rtdb.firebaseio.com/'
+  }
+)
+
+
 export default {
   // Gets all users
   getUsers: function() {
-    {
-      baseURL:'https://empdirr-default-rtdb.firebaseio.com/'
-    }
     return axios.get("https://randomuser.me/api/?results=200&nat=us");
   }
 };
